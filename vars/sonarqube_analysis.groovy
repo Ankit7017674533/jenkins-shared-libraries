4c8582dd7c9e5e06ff83886ca03dd6589bdeb8df
Ankit7017674533 <57895783+Ankit7017674533@users.noreply.github.com>
2025-02-10 19:19:11 +0530

    Update sonarqube_analysis.groovy

--- vars/sonarqube_analysis.groovy
+++ vars/sonarqube_analysis.groovy
@@ -1,12 +1,16 @@
 // vars/sonarqube_analysis.groovy
 def call(String sonarToolName, String projectKey, String projectName) {
-    def sonarHome = tool name: sonarToolName
-    withSonarQubeEnv(sonarToolName) {
-        sh """
-        ${sonarHome}/bin/sonar-scanner \
-        -Dsonar.projectKey=${projectKey} \
-        -Dsonar.projectName=${projectName} \
-        -Dsonar.sources=.
-        """
+    try {
+        def sonarHome = tool name: sonarToolName
+        withSonarQubeEnv(sonarToolName) {
+            sh """
+            ${sonarHome}/bin/sonar-scanner \
+            -Dsonar.projectKey=${projectKey} \
+            -Dsonar.projectName=${projectName} \
+            -Dsonar.sources=.
+            """
+        }
+    } catch (Exception e) {
+        error "SonarQube analysis failed: ${e.message}"
     }
 }
